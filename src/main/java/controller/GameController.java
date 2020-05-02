bb2a993c7f91f433b3fb86839e2cb997e6ad2bae
stp <stothp@gmail.com>
2020-05-02 13:06:12 +0200

    It is now possible to make moves.

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,10 +1,7 @@
 package controller;
 
 import com.sun.nio.sctp.PeerAddressChangeNotification;
-import gamelogic.BoardPosition;
-import gamelogic.InvalidPositionException;
-import gamelogic.PegSolitaire;
-import gamelogic.Tile;
+import gamelogic.*;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
@@ -27,6 +24,19 @@ public class GameController {
             "  OOO  "
     };
 
+    private static final String[] testDef = {
+            "  ...  ",
+            "  ...  ",
+            "..OOO..",
+            "..OOO..",
+            "..OOO..",
+            "  ...  ",
+            "  ...  "
+    };
+
+    private BoardPosition selection = null;
+    private ArrayList<BoardPosition> validSteps = null;
+
     @FXML
     private VBox vbox1;
 
@@ -38,7 +48,7 @@ public class GameController {
 
     @FXML
     public void initialize(){
-        solitaire = new PegSolitaire(englishBoardDef);
+        solitaire = new PegSolitaire(testDef);
         //mainContainer.getChildren().add(new Label("Heló"));
         //vbox1.setVgrow(pane1, Priority.ALWAYS);
         this.board = new BoardController(solitaire.getRowsCount(), solitaire.getColumsCount());
@@ -49,19 +59,48 @@ public class GameController {
             @Override
             public void handle(TileClickedEvent event) {
                 try {
-                    ArrayList<BoardPosition> validsteps = solitaire.getValidSteps(new BoardPosition(event.getRow(), event.getColumn()));
-                    if (validsteps.size() > 0) {
-                        board.setTileState(event.getRow(), event.getColumn(), TileController.State.SELECTED);
-                    }
-                    for (BoardPosition pos: validsteps){
-                        board.setTileState(pos.getRow(), pos.getColumn(), TileController.State.SELECTABLE);
-                    }
-                } catch (InvalidPositionException e){
+                    BoardPosition tileClicked = new BoardPosition(event.getRow(), event.getColumn());
 
+                    if (selection == null) {
+                        validSteps = solitaire.getValidSteps(tileClicked);
+                        if (validSteps.size() > 0) {
+                            board.setTileState(event.getRow(), event.getColumn(), TileController.State.SELECTED);
+                            for (BoardPosition pos: validSteps){
+                                board.setTileState(pos.getRow(), pos.getColumn(), TileController.State.SELECTABLE);
+                            }
+                            selection = tileClicked;
+                        }
+                        return;
+                    }
+
+                    if (validSteps.contains(tileClicked)){
+                        solitaire.performStep(selection, tileClicked);
+                        selection = null;
+                        validSteps = null;
+                        refreshBoard();
+                        return;
+                    }
+
+                    validSteps = solitaire.getValidSteps(tileClicked);
+                    if (validSteps.size() > 0) {
+                        refreshBoard();
+                        board.setTileState(event.getRow(), event.getColumn(), TileController.State.SELECTED);
+                        for (BoardPosition pos: validSteps){
+                            board.setTileState(pos.getRow(), pos.getColumn(), TileController.State.SELECTABLE);
+                        }
+                        selection = tileClicked;
+                        return;
+                    }
+
+                    selection = null;
+                    validSteps = null;
+                    refreshBoard();
+
+                } catch (InvalidPositionException e){
+                } catch (InvalidStepException e){
                 }
             }
         });
-        //board.setTileState(0, 0, TileController.State.SELECTED);
     }
 
     private void refreshBoard() {
